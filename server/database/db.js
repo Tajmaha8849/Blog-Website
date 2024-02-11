5e061913b0893c8be909aa61600cc70256ac3a18
Tajmaha8849 <shubhamprajapati9537@gmail.com>
2024-02-11 18:47:01 +0530

    first commit

---
+++ server/database/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const Connection = async () => {
+    const URL = `mongodb://user200:test1234@ac-nyo0him-shard-00-00.cqsyxhg.mongodb.net:27017,ac-nyo0him-shard-00-01.cqsyxhg.mongodb.net:27017,ac-nyo0him-shard-00-02.cqsyxhg.mongodb.net:27017/?ssl=true&replicaSet=atlas-84vf1f-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true })
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting to the database ', error);
+    }
+};
+
+export default Connection;
